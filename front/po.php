a3e3dab0de89d69ffd9a920ed042ff803ba06096
Hanna <hannawang003@gmail.com>
2024-01-05 15:27:07 +0800

    完成分類網誌的版面配置

--- front/po.php
+++ front/po.php
@@ -2,14 +2,29 @@
 .type-item {
     display: block;
 }
+
+.types,
+.new-items {
+    display: inline-block;
+    vertical-align: top;
+}
+
+.new-items {
+    width: 600px;
+}
 </style>
 <div class="nav">目前位置: 首頁 > 分類網誌 > <span class="type">健康新知</span></div>
-<fieldset>
+<fieldset class="types">
     <legend>分類網誌</legend>
     <a class='type-item'>健康新知</a>
     <a class='type-item'>菸害防治</a>
     <a class='type-item'>病防治</a>
     <a class='type-item'>慢性病防治</a>
+</fieldset>
+<fieldset class="new-items">
+    <legend>文章列表</legend>
+    <div class="list-items"></div>
+    <div class="article"></div>
 </fieldset>
 
 <script>
