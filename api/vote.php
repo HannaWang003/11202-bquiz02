dcb2525bac11972372e3a44a9b0b7bbaa37629f1
Hanna <hannawang003@gmail.com>
2024-01-08 15:07:03 +0800

    完成投票系統debug

--- api/vote.php
+++ api/vote.php
@@ -1,11 +1,10 @@
 <?php
 include_once "db.php";
-$_POST['opt'];
 $opt = $Que->find($_POST['opt']);
 $opt['vote']++;
 $Que->save($opt);
-$sub = $Que->find(['subject_id']);
+$sub = $Que->find($opt['subject_id']);
 $sub['vote']++;
 $Que->save($sub);
 
-to("../index.php?do=result&id={$sub['id']}");
+to("../index.php");
--- front/vote.php
+++ front/vote.php
@@ -5,12 +5,12 @@ $que = $Que->find($_GET['id']);
 <fieldset>
     <legend>目前位置:首頁 > 問卷調查 > <?= $que['text'] ?></legend>
     <h3> <?= $que['text'] ?></h3>
-    <form action="?do=result&id=<?= $que['id'] ?>" method="post">
+    <form action="./api/vote.php" method="post">
         <?php
         $opts = $Que->all(['subject_id' => $_GET['id']]);
         foreach ($opts as $opt) {
             echo "<div>";
-            echo "<input type='radio' name='' value='{$opt['id']}'}>";
+            echo "<input type='radio' name='opt' value='{$opt['id']}'}>";
             echo $opt['text'];
             echo "</div>";
         }
