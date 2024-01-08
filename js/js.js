0a7685d5a9466a2c058a3d9ec7cdc41a46dd168a
Hanna <hannawang003@gmail.com>
2024-01-08 15:21:09 +0800

    考式用reload()就可以了

--- js/js.js
+++ js/js.js
@@ -30,17 +30,17 @@ function good(news) {
       news,
     },
     () => {
-      // location.reload();
-      switch ($("#n" + news).text()) {
-        case "讚":
-          $("#n" + news).text("收回讚");
-          $("#g" + news).text($("#g" + news).text() * 1 + 1);
-          break;
-        case "收回讚":
-          $("#n" + news).text("讚");
-          $("#g" + news).text($("#g" + news).text() * 1 - 1);
-          break;
-      }
+      location.reload();
+      // switch ($("#n" + news).text()) {
+      //   case "讚":
+      //     $("#n" + news).text("收回讚");
+      //     $("#g" + news).text($("#g" + news).text() * 1 + 1);
+      //     break;
+      //   case "收回讚":
+      //     $("#n" + news).text("讚");
+      //     $("#g" + news).text($("#g" + news).text() * 1 - 1);
+      //     break;
+      // }
     }
   );
 }
